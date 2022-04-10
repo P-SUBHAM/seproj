d2576eb0587b459c7cbe2f00504e6cd0a37277d4
P-SUBHAM <p.subham.software@gmail.com>
2022-04-10 14:58:37 +0530

    clerk and manager menu updated, login working 14:58 10-04-2022

--- transportcpp/trn.cpp
+++ transportcpp/trn.cpp
@@ -40,13 +40,62 @@ class truck {
 
 
 };
+
+
 //////////////////////////////////////////////////
 
 vector<truck> trucks;
+map<string,string> mgrpsswd;
+map<string,string> clkpsswd;
 
 /////////////////////////////////////////////////
+bool authenticate(string usrtype, string username, string password) {
+    if(usrtype=="manager") {
+        if(mgrpsswd[username]==password) {
+            return true;
+        } else {
+            return false;
+        }
+    } else if(usrtype=="clerk") {
+        if(clkpsswd[username]==password) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
+
+bool authenticatemgr() {
+    string username, password;
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
+    if(authenticate("manager", username, password)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool authenticateclerk() {
+    string username, password;
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
+    if(authenticate("clerk", username, password)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int managermenuinp() {
-    cout<<"\n\n1. Add truck\n2. Remove truck\n3. Print all trucksv\n4. BACK\n5. EXIT\n";
+    cout<<"\n\n1. Add truck\n2. Remove truck\n3. Print all trucksv\n4. LOG-OUT!!!\n5. EXIT\n";
+    cout<<"\n";
     int choice;
     cin>>choice;
     cout<<"\n";
@@ -74,6 +123,70 @@ int managermenuinp() {
         default: {
             cout<<"Invalid choice\n";
             return managermenuinp();
+        }
+    }
+}
+
+int clerkmenuinp() {
+    cout<<"\n\n1. Register Consigment\n2. Dispatch Slip\n3. LOG-OUT!!!\n4. EXIT\n";
+    int choice;
+    cout<<"\n";
+
+    cin>>choice;
+    cout<<"\n";
+    switch(choice) {
+        case 1: {
+            return 1;
+            break;
+        }
+        case 2: {
+            return 2;
+            break;
+        }
+        case 3: {
+            return 3;
+            break;
+        }
+        case 4: {
+            return 4;
+            break;
+        }
+        default: {
+            cout<<"Invalid choice\n";
+            return clerkmenuinp();
+        }
+    }
+}
+
+int loginmenuinp() {
+    cout<<"\n\n1. Manager\n2. Clerk\n3. EXIT\n";
+    int choice;
+    cin>>choice;
+    cout<<"\n";
+    switch(choice) {
+        case 1: {
+            if(authenticatemgr() == false) {
+                cout<<"Invalid credentials\n";
+                return loginmenuinp();
+            }
+            return 1;
+            break;
+        }
+        case 2: {
+            if(authenticateclerk() == false) {
+                cout<<"Invalid credentials\n";
+                return loginmenuinp();
+            }
+            return 2;
+            break;
+        }
+        case 3: {
+            return 3;
+            break;
+        }
+        default: {
+            cout<<"Invalid choice\n";
+            return loginmenuinp();
         }
     }
 }
@@ -108,10 +221,11 @@ void printalltrucks() {
 }
 
 void loadinfo() {
+    /////////////////////////////////// TRUCK ///////////////////////////////////////////////
     fstream truckfile;
     truckfile.open("db/trucksdb.txt", ios::in);
     if(!truckfile) {
-        cout<<"File not found\n";
+        cout<<"File not found truckdb 1\n";
         return;
     }
     string line;
@@ -124,13 +238,34 @@ void loadinfo() {
         getline(ss, time);
         trucks.push_back(truck(stoi(id), stoi(capacity), stoi(speed), time));
     }
+    /////////////////////////////////// PASSWORD ///////////////////////////////////////////////
+    fstream passwordfile;
+    passwordfile.open("db/password.txt", ios::in);
+    if(!passwordfile) {
+        cout<<"File not found password 1\n";
+        return;
+    }
+    string line1;
+    while(getline(passwordfile, line1)) {
+        stringstream ss1(line1);
+        string usertype,username, password;
+        getline(ss1, usertype, ',');
+        getline(ss1, username, ',');
+        getline(ss1, password);
+        if(usertype == "manager") {
+            mgrpsswd[username] = password;
+        } else if(usertype == "clerk") {
+            clkpsswd[username] = password;
+        }
+    }
 }
 
 void saveinfo() {
+    cout<<"saving updates\n";
     fstream truckfile;
     truckfile.open("db/trucksdb.txt", ios::out);
     if(!truckfile) {
-        cout<<"File not found\n";
+        cout<<"File not found truckdb 2\n";
         return;
     }
     for(int i = 0; i < trucks.size(); i++) {
@@ -153,7 +288,6 @@ void managermenu(int choice) {
                 break;
             }
             case 4: {
-                //goback(); //TO DO TO-IMPLEMENT
                 break;
             }
             case 5: {
@@ -166,22 +300,59 @@ void managermenu(int choice) {
         }
 } 
 
+void clerkmenu(int choice) {
+    switch(choice) {
+            case 1: {
+                //regsiterconsignment();
+                cout<<"TO-DO Register Consignment\n";
+                break;
+            }
+            case 2: {
+                //generateDispatchSlip();
+                cout<<"TO-DO Dispatch Slip\n";
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                return;
+            }
+            default: {
+                cout<<"Invalid choice\n";
+                return clerkmenu(clerkmenuinp());
+            }
+        }
+} 
+
 int main() 
 {
     loadinfo();
-    
-    while(true) {
-        int choice = managermenuinp();
-        managermenu(choice);
-        if(choice == 4) break;
-        if(choice == 5) {saveinfo(); return 0;}
+    int cno=0; // cno = 0 means login page() auth
+    while(cno!=3) {
+        if(cno == 0) cno = loginmenuinp();
+        if(cno == 1) {
+            int choice = managermenuinp();
+            managermenu(choice);
+            if(choice == 4) {cno = 0;};
+            if(choice == 5) {saveinfo(); return 0;}
+        }
+        else if(cno == 2) {
+            int choice = clerkmenuinp();
+            clerkmenu(choice);
+            if(choice == 3) {cno = 0;};
+            if(choice == 4) {saveinfo(); return 0;}
+        }
+        else if(cno == 3) {
+            saveinfo();
+            return 0;
+        }
+        else {
+            cout<<"Invalid choice\n";
+        }
     }
-
-    cout<<"saving updates\n";
+    
     saveinfo();
-    cout<<"Saved info\n";
-    cout<<"Exiting\n";
-    cout<<"logging out\n";
 
     return 0;
 }
